7b61fcaec3a7ac559c499bfa5edcefaad59c468d
Keaton Neville <keaton@floatcard.com>
2026-02-26 19:23:16 -0500

    test

--- netsuitesdk/api/custom_segments.py
+++ netsuitesdk/api/custom_segments.py
@@ -9,10 +9,14 @@ class CustomSegments(ApiBase):
         ApiBase.__init__(self, ns_client=ns_client, type_name='CustomSegment')
 
     def get_all(self):
-        return self._get_all()
+        """
+        Get all custom segments using search API (getAll not supported for CustomSegment)
+        """
+        return self._search_all_generator(page_size=100)
 
     def get_all_generator(self, page_size=20):
         """
         Returns a generator which is more efficient memory-wise
+        Uses search API instead of getAll (which is not supported for CustomSegment)
         """
-        return self._get_all_generator()
+        return self.create_paginated_search(page_size=page_size)
